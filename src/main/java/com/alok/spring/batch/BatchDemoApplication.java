489dfd9a916f726af831ba9d18e73700cfbe19c4
alokkusingh <alok.ku.singh@gmail.com>
2020-04-29 13:53:29 +0530

    first commit

---
+++ src/main/java/com/alok/spring/batch/BatchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alok.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchDemoApplication.class, args);
+	}
+
+}
